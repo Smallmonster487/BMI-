55c019e9f30332bca387b87331fa88c0ecf1ab5d
m305-39\w401 <s11228246@gmail.com>
2018-03-15 17:52:30 +0800

    第一次上傳

---
+++ BMI計算機/BMI計算機/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI計算機
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tall;
+            double KG;
+
+            float BMI;
+            
+        }
+    }
+}
